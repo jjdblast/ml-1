3977441440935ec9b21dc014e51bc0cf8102aff8
Ecolss <loganecolss@gmail.com>
2014-10-23 22:26:35 +0800

    Add some pictures.

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 machine-learning
 ================
 
-Migrate some of my machine learning code from Dropbox (unavailable any longer in China) to github.
-The files in this repo contains some simple algorithms I implemented along the way of learning ML, and the code might be messy, so no intention to make it a off-the-shelf toolbox for the time being, ;-)
+Migrate some of machine learning code from Dropbox to github, maybe also trying to write some useful packages.
+
+# nn
+---
+* *ae.py*
+Contains sparse autoencoder (*SAE*) and denoising autoencoder (*DAE*).
+
+*SAE* is implemented with *lbfgs* as the training method, whereas *DAE* is trained with *sgd* (constant learning rate). They are easy to use (I think so ;P), you just specify the size of input and hidden layers and call *train* to start the training, I have some demo code in [test.py](nn/test.py) on MNIST dataset, here are some of the results of image filters.
+
+Denoising autoencoders with *corruption level=0.0*:
+![alt text](nn/pic/dae_filter_level_0.png)
+
+Denoising autoencoders with *corruption level=0.3*:
+![alt text](nn/pic/dae_filter_level_30.png)
+
+Denoising autoencoders with *corruption level=0.3* and *untied weightc*:
+![alt text](nn/pic/dae_filter_level_30_untied.png)
--- nn/test.py
+++ nn/test.py
@@ -1,9 +1,7 @@
 from scipy.io import loadmat
 from ae import *
 from nn_util import *
-from nn_net import *
 import time, pickle
-from utils import *
 from PIL import Image
 
 
@@ -13,10 +11,12 @@ def test_SAE_mnist():
     patches = sample_patches(10000, 8, images)
     patches.shape = (-1, 8*8)
 
-    x = normalize_3sigma(patches[:-5000])
-    x_validate = normalize_3sigma(patches[-5000:])
-    sae = SAE(8*8, 200, weight_decay=0, sparsity_control=0, sparsity=1e-2)
-    sae.train(x, x, 50, x_validate, x_validate, True)
+    x = normalize_3sigma(patches)
+    sae = SAE(8*8, 25, weight_decay=1e-4, sparsity_control=3, sparsity=1e-2)
+    sae.train(x, x, 400, x_validate=None, y_validate=None, display=False)
+    tmp = tile_images(sae.w0_, (8,8), (5,5), (1,1))
+    pl.imshow(tmp, cmap='gray')
+    pl.show()
 
 def test_DAE_mnist():    
     with open('../ufldl/data/mnist.pkl', 'rb') as fp:
@@ -30,64 +30,5 @@ def test_DAE_mnist():
     pl.imshow(tmp, cmap='gray')
     pl.show()
 
-def test_Net_grad_check():
-    net = Net([['full', 5, {}],
-               ['sig', 10, {}],
-               ['full', 10, {}],
-               ['sig', 5, {}]])
-    x = np.random.rand(10, 5)
-    y = np.random.rand(10, 5)
-
-    for i in range(2):
-        w0 = net.layers_[0].w_.copy()
-        b0 = net.layers_[0].b_.copy()
-        w1 = net.layers_[2].w_.copy()
-        b1 = net.layers_[2].b_.copy()
-        p = pack_params(w0, b0, w1, b1)
-        shape = (w0.shape, b0.shape, w1.shape, b1.shape)
-    
-        def cost(p, x, y):
-            ww0, bb0, ww1, bb1 = unpack_params(p, shape)
-            z0 = np.dot(x, ww0.T) + bb0
-            a0 = sigmoid(z0)
-            z1 = np.dot(a0, ww1.T) + bb1
-            a1 = sigmoid(z1)
-            c = 0.5 / x.shape[0] * np.sum((a1-y)**2)
-            return [c]
-    
-        grad_n = compute_numeric_grad(cost, p, x=x, y=y)
-        p -= 0.1 * (grad_n + 1.0 * p)
-    
-        net.fprop(x)
-        loss = net.calculate_loss(y)
-        net.bprop(loss)
-        net.update(0)
-        grad = pack_params(net.layers_[0].wgrad_, net.layers_[0].bgrad_, net.layers_[2].wgrad_, net.layers_[2].bgrad_)
-        p1 = pack_params(net.layers_[0].w_, net.layers_[0].b_, net.layers_[2].w_, net.layers_[2].b_)
-    
-        pl.plot(p)
-        pl.plot(p1)
-        pl.show()
-
-def test_Net_ae():
-    with open('../ufldl/data/mnist.pkl', 'rb') as fp:
-        mnist = pickle.load(fp)
-    images = mnist[0][0]
-    x = images
-
-    net = Net([['deno', 28*28, {'level': 0.3}],
-               ['full', 28*28, {}],
-               ['sig', 500, {}],
-               ['full', 500, {}],
-               ['sig', 28*28, {}]], base_lr=0.1, decay=0.0, momentum=0.0)
-
-    net.train(x, x, n_epochs=15, batch_size=20, loss_type=1, update_type=0, 
-                x_validate=None, y_validate=None, evaluate=False, display=False)
-    tmp = tile_images(net.layers_[1].w_, (28,28), (10,10))
-    pl.imshow(tmp, cmap='gray')
-    pl.show()
-
-
 if __name__ == '__main__':
-    test_Net_ae()
     test_DAE_mnist()
